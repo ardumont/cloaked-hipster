119a4191b8aec9acafbbc0911c20b456ab3e14b7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-01 23:13:01 +0200

    Initial commit

---
+++ test/cryto_challenge/core_test.clj
@@ -0,0 +1,7 @@
+(ns cryto-challenge.core-test
+  (:require [clojure.test :refer :all]
+            [cryto-challenge.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
