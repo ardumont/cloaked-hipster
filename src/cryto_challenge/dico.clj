f15b4af9ada9b46db760c8db0dd83e1b9d336977
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-02 16:18:06 +0200

    Formatting.

--- src/cryto_challenge/dico.clj
+++ src/cryto_challenge/dico.clj
@@ -3,43 +3,43 @@
   (:use [midje.sweet :only [fact future-fact]]))
 
 (def ^{:doc "base64 dictionary to encode in base64"}
-  base64 {0 \A
+  base64 {0  \A
           16 \Q
           32 \g
           48 \w
-          1 \B
+          1  \B
           17 \R
           33 \h
           49 \x
-          2 \C
+          2  \C
           18 \S
           34 \i
           50 \y
-          3 \D
+          3  \D
           19 \T
           35 \j
           51 \z
-          4 \E
+          4  \E
           20 \U
           36 \k
           52 \0
-          5 \F
+          5  \F
           21 \V
           37 \l
           53 \1
-          6 \G
+          6  \G
           22 \W
           38 \m
           54 \2
-          7 \H
+          7  \H
           23 \X
           39 \n
           55 \3
-          8 \I
+          8  \I
           24 \Y
           40 \o
           56 \4
-          9 \J
+          9  \J
           25 \Z
           41 \p
           57 \5
