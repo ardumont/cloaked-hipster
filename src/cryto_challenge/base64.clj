1f3d8ac8b5efb2ea82cff8e1fd47b1fa787b1314
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-02 14:49:15 +0200

    Refactoring and improve the coverage testing.

--- src/cryto_challenge/base64.clj
+++ src/cryto_challenge/base64.clj
@@ -17,18 +17,35 @@
   (comp24 [1 1 1 1 1 1 1 1 0 0 0 0 0 0 1 1])                 => [[1 1 1 1 1 1 1 1 0 0 0 0 0 0 1 1 0 0]             [\=]]
   (comp24 [1 1 1 1 1 1 1 1 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1]) => [[1 1 1 1 1 1 1 1 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1] []])
 
+(def to-bits ^{:private true
+               :doc "Transform a string into a list of bits."}
+  (partial mapcat (comp b/to-bin int)))
+
+(fact
+  (to-bits [\a \b \c]) => [0 1 1 0 0 0 0 1 0 1 1 0 0 0 1 0 0 1 1 0 0 0 1 1]
+  (to-bits "haskell")  => [0 1 1 0 1 0 0 0 0 1 1 0 0 0 0 1 0 1 1 1 0 0 1 1 0 1 1 0 1 0 1 1 0 1 1 0 0 1 0 1 0 1 1 0 1 1 0 0 0 1 1 0 1 1 0 0])
+
+(defn to-base64
+  "Given a 8 or 16 or 24-bits chunk, compute the bits sequence into base64."
+  [b]
+  (let [[part complement] (comp24 b)
+        p24               (->> part
+                               (partition 6)
+                               (map (comp d/base64 b/to-num)))]
+    (concat p24 complement)))
+
+(fact
+  (to-base64 [1 1 1 1 1 1 1 1 0 0 0 0])                         => [\/ \w]
+  (to-base64 [1 1 1 1 1 1 1 1 0 0 0 0 0 0 1 1 0 0])             => [\/ \w \M]
+  (to-base64 [1 1 1 1 1 1 1 1 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1]) => [\/ \w \P \/])
+
 (defn encode
   "ascii to base64"
   [s]
-  (->> (partition-all 3 s)                                                        ;; 3-words chunks (24 bits)
-       (mapcat (partial mapcat (comp b/to-bin int)))                              ;; transform into 8-bits words all concatenated
-       (partition-all 24)                                                         ;; 24-bits chunks
-       (mapcat (fn [b]                                                             ;; deal with the last chunk of bits (which can be of size 8, 16 or 24)
-                 (let [[part complement] (comp24 b)
-                       p24               (->> part
-                                              (partition 6)
-                                              (map (comp d/base64 b/to-num)))]
-                   (concat p24 complement))))
+  (->> (partition-all 3 s)       ;; 3-words chunks (24 bits)
+       (mapcat to-bits)          ;; transform into 8-bits words all concatenated
+       (partition-all 24)        ;; 24-bits chunks
+       (mapcat to-base64)        ;; deal with the last chunk of bits (which can be of size 8, 16 or 24)
        (clojure.string/join "")))
 
 (fact
