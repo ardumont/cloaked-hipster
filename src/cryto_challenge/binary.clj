8826e1700dd78493c96f370651e8f29bd1d42a02
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-02 11:46:39 +0200

    More significant name

--- src/cryto_challenge/binary.clj
+++ src/cryto_challenge/binary.clj
@@ -2,13 +2,13 @@
   "A binary namespace to deal with transformation into binary"
   (:use [midje.sweet :only [fact future-fact]]))
 
-;; bits sequence are read from the left to the right
+;; bits sequence are read from the left to the right (big endian)
 ;; significant first, least significant
 ;; Exemple:
 ;; head - most significant [0 0 0 0 0 1 1 1] - least significant - tail
 ;; [0 0 0 0 0 1 1 1] reads 7
 
-(defn make
+(defn comp-before
   "Complement a bit sequence to n bits (if necessary) - the bits are added by the most significant side (so by the head)."
   [n b]
   (->> (iterate #(concat [0] %) b)
@@ -16,10 +16,10 @@
        first))
 
 (fact
-  (make 8 [1 1 1])           => [0 0 0 0 0 1 1 1]
-  (make 8 [0 0 0 0 1 0 0 0]) => [0 0 0 0 1 0 0 0]
-  (make 4 [1 1 1])           => [0 1 1 1]
-  (make 10 [0 0 0 0 1 0 0 0]) => [0 0 0 0 0 0 1 0 0 0])
+  (comp-before 8 [1 1 1])            => [0 0 0 0 0 1 1 1]
+  (comp-before 8 [0 0 0 0 1 0 0 0])  => [0 0 0 0 1 0 0 0]
+  (comp-before 4 [1 1 1])            => [0 1 1 1]
+  (comp-before 10 [0 0 0 0 1 0 0 0]) => [0 0 0 0 0 0 1 0 0 0])
 
 (defn- to-bin
   "Convert a number into binary sequence"
@@ -34,7 +34,7 @@
   (to-bin 2)  => [1 0])
 
 (def bin ^{:doc "Given a number, compute its 8-bit representation"}
-  (comp (partial make 8) to-bin))
+  (comp (partial comp-before 8) to-bin))
 
 (fact
   (bin 97) => [0 1 1 0 0 0 0 1]
