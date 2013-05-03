5b0ee75577ee385f694a98a1340d1cc4c3685a30
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-03 17:28:13 +0200

    challenge 2 - correctness

---
+++ src/crypto/challenge2.clj
@@ -0,0 +1,31 @@
+(ns crypto.challenge2
+  "2. Fixed XOR
+Write a function that takes two equal-length buffers and produces their XOR sum.
+The string:
+ 1c0111001f010100061a024b53535009181c - hex encrypted?
+... after hex decoding, when xor'd against:
+ 686974207468652062756c6c277320657965 - hex not encrypted in base64 (hit the bull's eye)
+... should produce:
+ 746865206b696420646f6e277420706c6179 - hex not encrypted in base64 (the kids don't play)"
+  (:require [midje.sweet       :as m]
+            [crypto.challenge1 :as c1]
+            [crypto.byte       :as byte]
+            [crypto.binary     :as binary]
+            [crypto.ascii      :as ascii]
+            [crypto.base64     :as b64]
+            [crypto.hex        :as hex]))
+
+(defn xor
+  "compute the xor of 2 hex strings"
+  [h0 h1]
+  {:pre [(= (count h0) (count h1))]}
+  (->> [h0 h1]
+       (map (comp byte/to-bits ascii/to-bytes hex/decode))
+       (apply map bit-xor)
+       (partition 8)
+       (map binary/to-num)
+       hex/encode))
+
+(m/fact
+  (xor "abc" "defv")                                                                  => (m/throws AssertionError "Assert failed: (= (count h0) (count h1))")
+  (xor "1c0111001f010100061a024b53535009181c" "686974207468652062756c6c277320657965") => "746865206b696420646f6e277420706c6179")
--- src/crypto/hex.clj
+++ src/crypto/hex.clj
@@ -2,7 +2,8 @@
   "Hexadecimal namespace"
   (:require [midje.sweet    :as m]
             [clojure.string :as s]
-            [crypto.byte    :as byte]))
+            [crypto.byte    :as byte]
+            [crypto.ascii   :as ascii]))
 
 (defn- byte-to-hex
   "byte to hexadecimal"
@@ -13,13 +14,33 @@
 (m/fact
   (map byte-to-hex (range 0 20)) => ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "a" "b" "c" "d" "e" "f" "10" "11" "12" "13"])
 
-(defn encode
-  "String to hexadecimal string"
+(defn- encode-bytes
+  "Encode a bytes sequence into hex string"
+  [b]
+  (->> b
+       (map byte-to-hex)
+       (s/join "")))
+
+(m/fact
+  (encode-bytes (ascii/to-bytes "haskell rocks!"))                                   => "6861736b656c6c20726f636b7321"
+  (encode-bytes (ascii/to-bytes "I'm killing your brain like a poisonous mushroom")) => "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+
+(defmulti encode "Encode in hexadecimal a string or a sequence of bytes"
+  string?)
+
+;; byte sequence into hex string
+(defmethod encode false [b] (encode-bytes b))
+
+(m/fact
+  (encode (ascii/to-bytes "haskell rocks!"))                                   => "6861736b656c6c20726f636b7321"
+  (encode (ascii/to-bytes "I'm killing your brain like a poisonous mushroom")) => "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+
+;; String to hexadecimal string
+(defmethod encode true
   [s]
   (->> s
        byte/encode
-       (map byte-to-hex)
-       (s/join "")))
+       encode-bytes))
 
 (m/fact
   (encode "haskell rocks!")                                   => "6861736b656c6c20726f636b7321"
@@ -55,5 +76,7 @@
        (s/join "")))
 
 (m/fact
+  (decode "686974207468652062756c6c277320657965")                                                             => "hit the bull's eye"
+  (decode "746865206b696420646f6e277420706c6179")                                                             => "the kid don't play"
   (decode "6861736b656c6c20726f636b7321")                                                                     => "haskell rocks!"
   (decode "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d") => "I'm killing your brain like a poisonous mushroom")
