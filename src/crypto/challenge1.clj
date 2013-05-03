d602bfbcd92a7e8f17939ef31c1ce647e08a8828
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-03 11:04:07 +0200

    Correctness first

--- src/crypto/challenge1.clj
+++ src/crypto/challenge1.clj
@@ -7,12 +7,17 @@ should produce:
   SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
 Now use this code everywhere for the rest of the exercises. Here's a simple rule of thumb:
   Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing."
-  (:require [midje.sweet :as m]
-            [crypto.byte :as b]))
+  (:require [midje.sweet   :as m]
+            [crypto.byte   :as b]
+            [crypto.hex    :as h]
+            [crypto.base64 :as b64]))
 
 (defn encode "Encode an hexadecimal string into base64"
-  [s])
+  [s]
+  (-> s
+      h/from-hex
+      b64/encode))
 
-(m/future-fact
+(m/fact
   (encode "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
   => "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
