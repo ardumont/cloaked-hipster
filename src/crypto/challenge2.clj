b728a7d61ec46b388b91980701b14d7b6f0f6660
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-03 22:19:04 +0200

    Permit the computing of xor even if the length of the inputs mismatch.

--- src/crypto/challenge2.clj
+++ src/crypto/challenge2.clj
@@ -10,9 +10,10 @@ The string:
   (:require [midje.sweet       :as m]
             [crypto.binary     :as binary]
             [crypto.hex        :as hex]
-            [crypto.xor        :as xor]))
+            [crypto.xor        :as xor]
+            [clojure.string    :as s]))
 
-(defn xor
+(defn compute-xor
   "Compute xor of 2 hex strings"
   [h0 h1]
   (->> [h0 h1]
@@ -22,17 +23,21 @@ The string:
        (map binary/to-bytes)
        hex/encode))
 
-(m/fact :one-way
-  (xor "1c0111001f010100061a024b53535009181c" "686974207468652062756c6c277320657965") => "746865206b696420646f6e277420706c6179"
-  (xor "746865206b696420646f6e277420706c6179" "686974207468652062756c6c277320657965") => "1c0111001f010100061a024b53535009181c")
+(m/fact :one-way-and-back
+  (compute-xor "1c0111001f010100061a024b53535009181c" "686974207468652062756c6c277320657965") => "746865206b696420646f6e277420706c6179"
+  (compute-xor "746865206b696420646f6e277420706c6179" "686974207468652062756c6c277320657965") => "1c0111001f010100061a024b53535009181c")
 
-(defn xor-with-checks
-  "Compute the xor of 2 hex strings of same size."
-  [h0 h1]
-  {:pre [(= (count h0) (count h1))]}
-  (xor h0 h1))
+(defn xor
+  "Compute the xor of between the input s and the key key."
+  [s key]
+  {:pre [(>= (count s) (count key))]}
+  (->> key
+       cycle
+       (take (count s))
+       (compute-xor s)))
 
 (m/fact
-  (xor-with-checks "abc" "defv")                                                                  => (m/throws AssertionError "Assert failed: (= (count h0) (count h1))")
-  (xor-with-checks "1c0111001f010100061a024b53535009181c" "686974207468652062756c6c277320657965") => "746865206b696420646f6e277420706c6179"
-  (xor-with-checks "746865206b696420646f6e277420706c6179" "686974207468652062756c6c277320657965") => "1c0111001f010100061a024b53535009181c")
+  (xor "abcd" "defghi")                                                               => (m/throws AssertionError "Assert failed: (>= (count s) (count key))")
+  (xor "abcd" "de")                                                                   => "7513"
+  (xor "1c0111001f010100061a024b53535009181c" "686974207468652062756c6c277320657965") => "746865206b696420646f6e277420706c6179"
+  (xor "746865206b696420646f6e277420706c6179" "686974207468652062756c6c277320657965") => "1c0111001f010100061a024b53535009181c")
