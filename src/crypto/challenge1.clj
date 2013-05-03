31582a70941620374bd88dad96f290bd956f9e81
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-03 11:09:12 +0200

    decode base64 into hexadecimal

--- src/crypto/challenge1.clj
+++ src/crypto/challenge1.clj
@@ -21,3 +21,14 @@ Now use this code everywhere for the rest of the exercises. Here's a simple rule
 (m/fact
   (encode "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
   => "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+
+(defn decode
+  "Decode a base64 encoded string into an hexadecimal string"
+  [s]
+  (-> s
+      b64/decode
+      h/to-hex))
+
+(m/fact
+  (decode "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+  => "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
