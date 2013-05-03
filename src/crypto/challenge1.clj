c5e67b0c2be2168c4c2f7dbd1f13b40ed400c4f6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-03 10:51:10 +0200

    Formatting

--- src/crypto/challenge1.clj
+++ src/crypto/challenge1.clj
@@ -5,14 +5,14 @@ The string:
   49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
 should produce:
   SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
-Now use this code everywhere for the rest of the exercises. Here's a
-simple rule of thumb:
+Now use this code everywhere for the rest of the exercises. Here's a simple rule of thumb:
   Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing."
-  (:use [midje.sweet :only [fact future-fact contains]]))
+  (:require [midje.sweet :as m]
+            [crypto.byte :as b]))
 
-(defn encode ""
+(defn encode "Encode an hexadecimal string into base64"
   [s])
 
-(fact
+(m/future-fact
   (encode "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
   => "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
