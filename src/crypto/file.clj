fd8ba8dd0c3745308b46deccd70c9e40edeee08c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-04 18:41:41 +0200

    Adding means to load the base64 encoded string

--- src/crypto/file.clj
+++ src/crypto/file.clj
@@ -3,9 +3,15 @@
   (:require [midje.sweet    :as m]
             [clojure.string :as s]))
 
+(defn load-simple
+  [filepath]
+  (-> filepath
+      slurp
+      (s/replace "\n" "")))
+
 (defn load
   "Given a file, load the content of the file and return each line into a vector of lines."
-  [f]
-  (-> f
-      slurp
+  [filepath]
+  (-> filepath
+      load-simple
       s/split-lines))
