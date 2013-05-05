3e191162a071f469270e7fbf626b19f46b698f3d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-05 23:50:09 +0200

    Intermediary commit

--- src/crypto/c6.clj
+++ src/crypto/c6.clj
@@ -30,6 +30,7 @@ e. For each block, the single-byte XOR key that produces the best looking histog
  repeating-key XOR key byte for that block. Put them together and you have the key."
   (:require [midje.sweet      :as m]
             [crypto.c1        :as c1]
+            [crypto.c2        :as c2]
             [crypto.xor       :as xor]
             [crypto.distance  :as distance]
             [crypto.file      :as file]
@@ -37,36 +38,43 @@ e. For each block, the single-byte XOR key that produces the best looking histog
             [crypto.ascii     :as ascii]
             [crypto.hex       :as hex]
             [crypto.frequency :as frequency]
+            [crypto.char      :as char]
+            [clojure.string   :as s]
             [incanter
              [core            :as icore]
              [charts          :as icharts]
              [datasets        :as idata]]))
 
-(def byte-input (-> "./resources/base64-encoded"
+(def hex-input (-> "./resources/base64-encoded"
                      file/load-simple
-                     c1/decode
-                     hex/to-bytes))
+                     c1/decode))
+
+(def byte-input (hex/to-bytes hex-input))
 
 (defn block
   "Compute 2 n-block chars, [0..n] and [n..n+1]"
-  [n s]
-  [(take n s) (->> s (drop n) (take n))])
+  [s n v]
+  (let [vect (vec v)]
+    [(subvec vect s (+ s n)) (subvec vect (+ s n) (+ s (* 2 n)))]))
 
 (m/fact
-  (block 3 (ascii/to-bytes "hello, dude")) => [[104 101 108] [108 111 44]]
-  (block 3 "hello world!")                 => [[\h \e \l] [\l \o \space]]
-  (block 7 "hello world!")                 => [[\h \e \l \l \o \space \w] [\o \r \l \d \!]])
+  (block 0 3 (ascii/to-bytes "hello, dude")) => [[104 101 108] [108 111 44]]
+  (block 0 3 "hello world!")                 => [[\h \e \l] [\l \o \space]]
+  (block 0 6 "hello world!")                 => [[\h \e \l \l \o \space] [\w \o \r \l \d \!]]
+  (block 2 2 "hello world!")                 => [[\l \l] [\o \space]])
 
 (defn normalize
-  "Normalize the hamming distance between 2 n-blocks from the sequence s."
+  "Normalize the hamming distance between 4 n-blocks from the sequence s."
   [n s]
-  (let [d (->> s
-          (block n)
-          (apply distance/hamming))]
+  (let [bs [(block 0 n s) (block n n s) (block (* 2 n) n s) (block (* 3 n) n s)]
+        d (/ (->> bs
+                  (map (fn [[bs be]] (distance/hamming bs be)))
+                  (apply +))
+             4)]
     (/ d n)))
 
 (m/fact
-  (normalize 2 (ascii/to-bytes "hello")) => 3/2)
+  (normalize 2 (ascii/to-bytes "hello world")) => 19/8)
 
 (defn potential-key-sizes
   "Given the input, return the potential sizes (in natural order)"
@@ -88,24 +96,14 @@ e. For each block, the single-byte XOR key that produces the best looking histog
 (comment
   (all-blocks byte-input 2))
 
-(defn mean
-  [vals]
-  (let [n 4]
-    (->
-     (->> vals (take n) (apply +))
-     (/ n)
-     float
-     Math/round)))
-
-(m/fact
-  (mean (-> byte-input potential-key-sizes vals)) => 2)
-
 (defn potential-keys
   "Given the byte-input, compute the potential key size and return the list of those possible keys with such sizes."
   [byte-input]
-  (let [key-size (mean (-> byte-input
-                           potential-key-sizes
-                           vals))]
+  (let [key-size (-> byte-input
+                     potential-key-sizes
+                     vals
+                     first
+                     int)]
     (all-blocks byte-input key-size)))
 
 (defn shift
@@ -124,21 +122,32 @@ e. For each block, the single-byte XOR key that produces the best looking histog
 (defn inject-block
   "Given an input of data, inject a block of data at the nth position"
   [n block data]
-  (let [l (count block)]
-    (->> data
-         (shift n)
-         (interleave (cycle [block])))))
+  (->> data
+       (map (fn [seq]
+              (->> seq
+                   (split-at n)
+                   (#(let [[h t] %]
+                       (concat h (conj t block))))
+                   flatten)))))
 
 (m/fact
-  ;; (inject-block 0 [1 2 3] [:a :b :c :d :e :f]) => [1 2 3 :a :b :c 1 2 3 :d :e :f]
-  ;; (inject-block 1 [1 2 3] [:a :b :c :d :e :f]) => [1 2 3 :d :e :f 1 2 3 :a :b :c]
-  ;; (inject-block 2 [1 2 3] [:a :b :c :d :e :f]) => [1 2 3 :a :b :c 1 2 3 :d :e :f]
-  ;; (inject-block 2 [1 2]   [:a :b :c :d :e :f]) => [1 2 :e :f 1 2 :a :b 1 2 :c :d]
-  (inject-block 0 [1 :a]  [[1 :a] [2 :b] [3 :c]]) => [[1 :a] [1 :a] [1 :a] [2 :b] [1 :a] [3 :c]]
-  (inject-block 1 [1 :a]  [[1 :a] [2 :b] [3 :c]]) => [[1 :a] [2 :b] [1 :a] [3 :c] [1 :a] [1 :a]]
-  (inject-block 2 [1 :a]  [[1 :a] [2 :b] [3 :c]]) => [[1 :a] [3 :c] [1 :a] [1 :a] [1 :a] [2 :b]])
+  (inject-block 0 [1 :a] [[1 :a] [2 :b] [3 :c]]) => [[1 :a 1 :a] [1 :a 2 :b] [1 :a 3 :c]]
+  (inject-block 1 [1 :a] [[1 :a] [2 :b] [3 :c]]) => [[1 1 :a :a] [2 1 :a :b] [3 1 :a :c]]
+  (inject-block 2 [1 :a] [[1 :a] [2 :b] [3 :c]]) => [[1 :a 1 :a] [2 :b 1 :a] [3 :c 1 :a]]
+  (inject-block 1 [2 :b] [[1 :a] [2 :b] [3 :c]]) => [[1 2 :b :a] [2 2 :b :b] [3 2 :b :c]]
+  (inject-block 2 [3 :c] [[1 :a] [2 :b] [3 :c]]) => [[1 :a 3 :c] [2 :b 3 :c] [3 :c 3 :c]])
 
-;; e. Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
+(defn compute-possible-transpositions
+  "Given a blocks, compute all possible transpositions."
+  [blocks]
+  (->> blocks
+       (map-indexed
+        (fn [i k] (inject-block i k blocks)))))
+
+(m/fact
+  (compute-possible-transpositions [[1 :a] [2 :b] [3 :c]]) => [[[1 :a 1 :a] [1 :a 2 :b] [1 :a 3 :c]]
+                                                               [[1 2 :b :a] [2 2 :b :b] [3 2 :b :c]]
+                                                               [[1 :a 3 :c] [2 :b 3 :c] [3 :c 3 :c]]])
 
 ;; f. Now transpose the blocks: make a block that is the first byte of every block, and a block
 ;; that is the second byte of every block, and so on.
@@ -148,17 +157,18 @@ e. For each block, the single-byte XOR key that produces the best looking histog
 ;; e. For each block, the single-byte XOR key that produces the best looking histogram is the
 ;;  repeating-key XOR key byte for that block. Put them together and you have the key.
 
-(defn compute
-  "Given a blocks, compute all possible transpositions."
-  [blocks]
-  (->> blocks
-       (map-indexed
-        (fn [i k] (inject-block i k blocks)))))
+(comment
+  (def blks (potential-keys byte-input))
+  (def blk (first blks))
 
-(m/fact
-  (compute [[1 :a] [3 :b] [5 :b]]) => [[[1 :a] [1 :a] [1 :a] [3 :b] [1 :a] [5 :b]]
-                                       [[3 :b] [3 :b] [3 :b] [5 :b] [3 :b] [1 :a]]
-                                       [[5 :b] [5 :b] [5 :b] [1 :a] [5 :b] [3 :b]]])
+  (def all-transposed-blocks (compute-possible-transpositions blks))
+  (ffirst all-transposed-blocks)
+
+  (def block-by-block
+    (->> all-transposed-blocks
+         (map (fn [bl]
+                (map xor/decrypt-brute-force bl)))))
+)
 
 (defn freq
   "For a given key, compute the frequencies of xor this key with all the other blocks"
@@ -171,13 +181,13 @@ e. For each block, the single-byte XOR key that produces the best looking histog
 
 ;;     Determine how long the key is
 
-;; This is done by XORing the encrypted data with itself shifted various numbers of places, and examining how many bytes are the same. If the bytes that are equal are greater than a certain percentage (6% accoridng to Bruce Schneier's Applied Cryptography second edition), then you have shifted the data by a multiple of the keylength. By finding the smallest amount of shifting that results in a large amount of equal bytes, you find the keylength.
+;; This is done by XORing the encrypted data with itself shifted various numbers of places, and examining how many bytes are the same.
+;; If the bytes that are equal are greater than a certain percentage (6% accoridng to Bruce Schneier's Applied Cryptography second edition),
+;; then you have shifted the data by a multiple of the keylength. By finding the smallest amount of shifting that results in a large amount of equal bytes, you find the keylength.
 
 ;;     Shift the cipher text by the keylength, and XOR against itself.
 
 ;; This removes the key and leaves you with the plaintext XORed with the plaintext shifted the length of the key. There should be enough plaintext to determine the message content.
-
-;; Section 8.2 of the Sci.crypt FAQ has more details on cracking repeated-key ciphers.
 
 (defn draw
   "Draw the bar chart for the given data {:col0 key :col1 freq-of-this-key} "
