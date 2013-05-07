60cd3158f51a268474c18c5ac0e0d424068f588f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-07 08:11:08 +0200

    Some change in the api to be able to specify the range (which vary according to inputs)

--- src/crypto/c6.clj
+++ src/crypto/c6.clj
@@ -63,46 +63,92 @@ e. For each block, the single-byte XOR key that produces the best looking histog
   (block 0 6 "hello world!")                 => [[\h \e \l \l \o \space] [\w \o \r \l \d \!]]
   (block 2 2 "hello world!")                 => [[\l \l] [\o \space]])
 
-(defn normalize
-  "Normalize the hamming distance between 4 n-blocks from the sequence s."
+(defn make-4blocks
+  "Make nb-blocks of size n with the string s. If nb-blocks is too large, return by convention 4 blocks."
   [n s]
-  (let [bs [(block 0 n s) (block n n s) (block (* 2 n) n s) (block (* 3 n) n s)]
-        d (/ (->> bs
-                  (map (fn [[bs be]] (distance/hamming bs be)))
-                  (apply +))
-             4)]
-    (/ d n)))
+  (for [i (range 0 4)] (block (* n i) n s)))
 
 (m/fact
-  (normalize 2 (ascii/to-bytes "hello world")) => 19/8)
+  (make-4blocks 2 "hello world, will be broke into multiple blocks.") => [[[\h \e] [\l \l]]
+                                                                          [[\l \l] [\o \space]]
+                                                                          [[\o \space] [\w \o]]
+                                                                          [[\w \o] [\r \l]]]
+  (make-4blocks 5 "little by little, we close the line ")             => [[[\l \i \t \t \l] [\e \space \b \y \space]]
+                                                                          [[\e \space \b \y \space] [\l \i \t \t \l]]
+                                                                          [[\l \i \t \t \l] [\e \, \space \w \e]]
+                                                                          [[\e \, \space \w \e] [\space \c \l \o \s]]])
 
-(defn potential-key-sizes
-  "Given the input, return the potential sizes (in natural order)"
-  [byte-input]
-  (->> (range 2 41)
+(defn normalize
+  "Normalize the hamming distance between 4 n-blocks from the sequence s."
+  [n byte-input]
+  (let [blks (make-4blocks n byte-input)                       ;;  c. For each KEYSIZE (n), take the FIRST n [0..n] worth of bytes, and the SECOND n [n+1..2*n] worth of bytes,
+                                                               ;; (Or take 4 KEYSIZE blocks instead of 2 and average the distances.)
+        l    (count blks)
+        c    (->> blks
+                  (map (fn [[bs be]] (distance/hamming bs be))) ;; and find the edit distance between them.
+                  (apply +))
+        mean-distance (/ c l)]
+    (/ mean-distance n)))                                      ;; Normalize this result by dividing by KEYSIZE.
+
+(m/fact
+  (normalize 2 (ascii/to-bytes "hello world, this must be long enough!")) => 19/8
+  (normalize 3 (ascii/to-bytes "hello world, this must be long enough!")) => 31/12)
+
+(defn keysize
+  "Given the input, return the potential keysize."
+  [byte-input range-test]
+  (->> range-test                                      ;; a. Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
        (reduce
-        (fn [m n] (assoc m n (normalize n byte-input)))
-        (sorted-map))))
+        (fn [m n] (assoc m n (normalize n byte-input))) ;; c. For each KEYSIZE, take the FIRST KEYSIZE [0..KEYSIZE] worth of bytes, and the SECOND KEYSIZE [KEYSIZE+1..2*KEYSIZE] worth
+                                                       ;; of bytes, and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
+        {})
+       (apply min-key second)                          ;; d. The KEYSIZE with the smallest normalized
+       first))                                         ;;  hamming/edit distance is probably the key.
 
 (comment
-  (potential-key-sizes byte-input))
+  (keysize byte-input (range 2 41)))
 
 (defn all-blocks
-  "Given a byte input and a key size, return the list of hex-encoded blocks with such size"
+  "Given a byte input and a key size, return the list of byte blocks with such size"
   [byte-input key-size]
   (->> byte-input
-       (partition key-size)))
+       (partition key-size))) ;; e. Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
 
 (comment
-  (all-blocks byte-input 2))
+  (all-blocks byte-input 2)
+
+  ;; I don't succeed in breaking, so i test with things i know!
+
+  (def encrypted-msg (-> {:key "secret"
+                          :msg "this is a test with sufficient length to have data to break"}
+                         xor/encrypt
+                         hex/to-bytes))
+
+  (count encrypted-msg)
+
+  ;; check that i can decrypt - OK
+  (byte/to-ascii (xor/xor encrypted-msg (ascii/to-bytes "secret")))
+
+  ;; now trying to break it
+
+  ;; first determine the keysize...
+  (keysize encrypted-msg (range 2 12))
+
+  (->> (range 2 12)
+       (reduce
+        (fn [m n] (assoc m n (normalize n encrypted-msg)))
+        {})
+
+       )
+
+  ;; boum - 2 this is false, the keysize is 6
+
+  )
 
 (defn potential-keys
   "Given the byte-input, compute the potential key size and return the list of those possible keys with such sizes."
-  [byte-input]
-  (let [key-size (-> byte-input
-                     potential-key-sizes
-                     keys
-                     first)]
+  [byte-input range-test]
+  (let [key-size (keysize byte-input range-test)]
     (all-blocks byte-input key-size)))
 
 (defn shift
@@ -140,6 +186,14 @@ e. For each block, the single-byte XOR key that produces the best looking histog
   (inject-block 1 [2 :b] [[1 :a] [2 :b] [3 :c]]) => [[1 2 :b :a] [2 2 :b :b] [3 2 :b :c]]
   (inject-block 2 [3 :c] [[1 :a] [2 :b] [3 :c]]) => [[1 :a 3 :c] [2 :b 3 :c] [3 :c 3 :c]])
 
+;; f. Now transpose the blocks: make a block that is the first byte of every block, and a block
+;; that is the second byte of every block, and so on.
+
+;; g. Solve each block as if it was single-character XOR. You already have code to do this.
+
+;; e. For each block, the single-byte XOR key that produces the best looking histogram is the
+;;  repeating-key XOR key byte for that block. Put them together and you have the key.
+
 (defn compute-possible-transpositions
   "Given a blocks, compute all possible transpositions."
   [blocks]
@@ -162,7 +216,7 @@ e. For each block, the single-byte XOR key that produces the best looking histog
 ;;  repeating-key XOR key byte for that block. Put them together and you have the key.
 
 (comment
-  (def blks (potential-keys byte-input))
+  (def blks (potential-keys byte-input (range 2 41)))
 
   (def all-transposed-blocks (compute-possible-transpositions blks))
 
@@ -172,6 +226,7 @@ e. For each block, the single-byte XOR key that produces the best looking histog
   (def block-by-block
     (->> all-transposed-blocks
          (map (partial map xor/decrypt-brute-force))))
+
 )
 
 (defn freq
@@ -216,12 +271,12 @@ e. For each block, the single-byte XOR key that produces the best looking histog
 
 (comment
   (def all-data ^{:doc "all potential data to graph"}
-    (let [blocks (potential-keys byte-input)]
+    (let [blocks (potential-keys byte-input (range 2 41))]
       (map (fn [key] (freq key blocks)) blocks)))
 
   (draw (nth all-data 255))
 
-  (let [blocks (potential-keys byte-input)]
+  (let [blocks (potential-keys byte-input (range 2 41))]
     (->> blocks
          (map (fn [key] (freq key blocks)))
          (filter (fn [m]
