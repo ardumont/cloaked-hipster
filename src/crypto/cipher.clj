29cd1106985981359710f32448dcbe1559280d86
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-18 13:41:46 +0200

    Given a file, encode/decode with one-time pad

--- src/crypto/cipher.clj
+++ src/crypto/cipher.clj
@@ -1,16 +1,19 @@
 (ns crypto.cipher
   "Encipher/Decipher files"
-  (:require [midje.sweet :as m]
+  (:require [midje.sweet     :as m]
+            [clojure.java.io :as io]
+            [clojure.string  :as string]
             [crypto
-             [hex        :as hex]
-             [binary     :as binary]
-             [frequency  :as frequency]
-             [byte       :as byte]
-             [xor        :as xor]
-             [ascii      :as ascii]]))
+             [hex            :as hex]
+             [binary         :as binary]
+             [frequency      :as frequency]
+             [byte           :as byte]
+             [xor            :as xor]
+             [file           :as file]
+             [ascii          :as ascii]]))
 
 (defn otp
-  "One-time pad"
+  "One-time pad a ascii message."
   [msg]
   (let [k (-> msg count byte/rand-bytes)]
     {:key k
@@ -20,3 +23,25 @@
 (m/fact
   (xor/decrypt
    (otp "may the repl be with you")) => "may the repl be with you")
+
+(defn otp-file!
+  "One-time pad with a ascii file. This will generate a file encoded with a one-time pad and another one with the key."
+  [filepath]
+  (let [{:keys [key msg]} (-> filepath
+                              slurp
+                              otp)
+        k (byte/>ascii key)
+        m (byte/>ascii msg)
+        fname (-> filepath io/file .getPath)]
+    (do
+      (spit (file/new-name fname (str "-encoded")) m)
+      (spit (file/new-name fname (str "-key")) k))))
+
+(defn otp-read!
+  "Given a file with one-time pad encoded, this will find the key file and decipher the file."
+  [filepath filekey]
+  (let [msg (slurp filepath)
+        key (slurp filekey)
+        res (xor/decrypt {:key key
+                          :msg (ascii/to-bytes msg)})]
+    (spit (file/new-name filepath (str "-then-decoded")) res)))
--- src/crypto/file.clj
+++ src/crypto/file.clj
@@ -1,18 +1,57 @@
 (ns crypto.file
-  "Manipulating file"
+  "File manipulations."
+  (:refer-clojure :exclude [name])
   (:require [midje.sweet    :as m]
-            [clojure.string :as s]))
+            [clojure.string :as string]
+            [clojure.java.io :as io]))
+
+(defn basename
+  [filepath]
+  (->> (string/split filepath #"/")
+       butlast
+       (string/join "/")))
+
+(defn name
+  "Given a filename, return its extension."
+  [filename]
+  (string/join "."
+               (-> filename
+                   (string/split #"\.")
+                   butlast)))
+
+(m/fact
+  (name "test.middle.another.txt") => "test.middle.another")
+
+(defn ext
+  "Given a filename, return its extension."
+  [filename]
+  (-> filename
+      (string/split #"\.")
+      last))
+
+(m/fact
+  (ext "test.middle.another.txt") => "txt")
 
 (defn ld-simple
   "Given a file, load its content and glue all lines together."
   [filepath]
   (-> filepath
       slurp
-      (s/replace "\n" "")))
+      (string/replace "\n" "")))
 
 (defn ld
   "Given a file, load the content of the file and return each line into a vector of lines."
   [filepath]
   (-> filepath
       slurp
-      s/split-lines))
+      string/split-lines))
+
+(defn new-name
+  "Given a filepath, build a new name respecting the same path."
+  [fname k]
+  (let [ext (ext fname)
+        name (name fname)]
+    (str name k "." ext)))
+
+(m/fact
+  (new-name "/home/test/this/is/a/path/to/this-file.middle.txt" "-key") => "/home/test/this/is/a/path/to/this-file.middle-key.txt")
