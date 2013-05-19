2134c4de4731d56b991c789582d583345ba14610
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-19 20:39:21 +0200

    Improve

--- decrypt.sh
+++ decrypt.sh
@@ -1,8 +1,12 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 FILES=$(cat files-decrypt)
 
-lein run -m crypto.cipher/-main -D "$FILES"
+set +e
 
-# if decrypted, then we remode the key
+ls ./src/crypto/c*-key.clj
+
+[ $? -eq 0 ] && set -e && lein run -m crypto.cipher/-main -D "$FILES"
+
+# if decrypted, then we remove the key
 rm ./src/crypto/c*-key.clj
