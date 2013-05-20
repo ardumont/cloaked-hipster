bb89f0eb576ea611179166b2750eb2ef48af9c48
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-20 09:42:03 +0200

    Some fooling around.

--- src/crypto/scratch.clj
+++ src/crypto/scratch.clj
@@ -1,2 +1,13 @@
 (ns crypto.scratch
-  (:require [midje.sweet :as m]))
+  (:require [midje.sweet :as m]
+            [crypto
+             [ascii      :as ascii]
+             [base64     :as b64]
+             [hex        :as hex]
+             [byte       :as byte]]))
+
+(-> "may the repl be with you" ascii/>bytes byte/>hex)
+"6d617920746865207265706c206265207769746820796f75"
+
+
+(b64/>hex "bWF5IHRoZSByZXBsIGJlIHdpdGggeW91")"6d617920746865207265706c206265207769746820796f75"
